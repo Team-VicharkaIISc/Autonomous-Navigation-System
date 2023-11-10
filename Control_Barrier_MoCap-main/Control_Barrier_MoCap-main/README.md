b746a09c35195b93306836aed58b9b1129e54c31
Sahil Chaudhary <63643403+sahil-chaudhary@users.noreply.github.com>
2023-11-10 23:23:30 +0530

    Delete Control_Barrier_MoCap-main/Control_Barrier_MoCap-main directory

--- Control_Barrier_MoCap-main/Control_Barrier_MoCap-main/README.md
+++
@@ -1,4 +0,0 @@
-# Control_Barrier_MoCap
-This Repository contains implementation of control barrier as a safety net to test various algorithms.
-
-Add Demo Videos on Youtube for the Control Barrier+Scots.
--- Control_Barrier_MoCap-main/Control_Barrier_MoCap-main/barrier_controller/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-setup_args = generate_distutils_setup(
-	packages=['barrier_controller'],
-	package_dir={'': 'include'}
-)
-
-setup(**setup_args)
